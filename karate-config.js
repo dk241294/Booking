3388503919763103f26f0ef0f037671cca53f090
Deepak <deepak@emtech.com>
2023-09-03 21:06:23 +0530

    Check API Health
    Service is up or down

---
+++ karate-config.js
@@ -0,0 +1,11 @@
+function fn() {
+    var config = {
+      baseUrl: 'https://restful-booker.herokuapp.com',
+      authToken: null,
+      defaultAuthToken:'Basic YWRtaW46cGFzc3dvcmQxMjM=',
+      bookingId: 578,
+      // createdBookingId: null
+    }
+    return config;
+   
+  }
