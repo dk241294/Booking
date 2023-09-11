f6a11e11945cd79ef0e7b173c532fb541b70ecf0
Deepak Kumar <deepak24121994@gmail.com>
2023-09-11 19:19:56 +0530

    Deleted the Script

--- karate-config.js
+++
@@ -1,9 +0,0 @@
-function fn() {
-    var config = {
-      baseUrl: 'https://restful-booker.herokuapp.com',
-      defaultAuthToken:'Basic YWRtaW46cGFzc3dvcmQxMjM=',
-      bookingId: 95
-    }
-    return config;
-   
-  }
